8638e5464ae98b248a71584cf14928145210f532
Gustavo Delbon Souza <61765239+MrDelbon@users.noreply.github.com>
2022-12-01 00:24:46 -0300

    Delete Padrões Comportamentais/Iterator/Exemplo de Código directory

--- Padrões Comportamentais/Iterator/Exemplo de Código/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main
-{
-	public static void main(String args[])
-	{
-		NotificationCollection nc = new NotificationCollection();
-		NotificationBar nb = new NotificationBar(nc);
-		nb.printNotifications();
-	}
-}
