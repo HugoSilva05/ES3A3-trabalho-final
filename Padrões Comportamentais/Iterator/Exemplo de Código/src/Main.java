ba707d81d779a6440cb7ea4b90ef147626281988
Gustavo Delbon Souza <61765239+MrDelbon@users.noreply.github.com>
2022-11-30 21:51:27 -0300

    Padrões Comportamentais

---
+++ Padrões Comportamentais/Iterator/Exemplo de Código/src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String args[])
+	{
+		NotificationCollection nc = new NotificationCollection();
+		NotificationBar nb = new NotificationBar(nc);
+		nb.printNotifications();
+	}
+}
